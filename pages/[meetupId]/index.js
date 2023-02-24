5f7252dc525f6c8d4ced2226ec9ec3f24b23a2ba
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-24 09:27:36 +0300

    Added teh address markup property

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -4,6 +4,7 @@ const MeetupDetailsPage = () =>
         <>
             <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/NBO5.jpg/267px-NBO5.jpg' alt='Nairobi Sunset' />
             <h1>Meetup at Nairobi</h1>
+            <address>Some address</address>
             <p>We will be meeting up in Nairobi at sunset</p>
         </>
     )
