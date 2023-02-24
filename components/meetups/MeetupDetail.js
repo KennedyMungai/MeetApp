a47bf2e45d8a5285c0c8224c999cb03ef84273f4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-24 10:28:47 +0300

    Also made alt dynamic

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -2,7 +2,10 @@ const MeetupDetail = (props) =>
 {
     return (
         <>
-            <img src={props.image} alt='Nairobi Sunset' />
+            <img
+                src={props.image}
+                alt={props.title}
+            />
             <h1>Meetup at Nairobi</h1>
             <address>Some address</address>
             <p>We will be meeting up in Nairobi at sunset</p>
