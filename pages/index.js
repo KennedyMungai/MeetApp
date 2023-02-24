b954d9e6f6841902fb9c3d34b73964929b31d560
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-24 08:38:11 +0300

    Added an image to the object

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 import MeetupList from "../components/meetups/MeetupList"
 
+const DUMMY_MEETUPS =
+    [
+        {
+            id: 'm1',
+            title: 'A first meetup',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/COLLECTIE_TROPENMUSEUM_Zicht_op_Nairobi_met_op_de_voorgrond_het_stadspark_met_tribune_TMnr_20014416.jpg/250px-COLLECTIE_TROPENMUSEUM_Zicht_op_Nairobi_met_op_de_voorgrond_het_stadspark_met_tribune_TMnr_20014416.jpg'
+        }
+    ]
+
 const HomePage = () =>
 {
     return (
