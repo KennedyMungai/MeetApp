94e009a1a3375d88024c9b30ed769b017b645802
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-24 10:28:17 +0300

    Changed the spelling of the prop item being accessed

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -2,7 +2,7 @@ const MeetupDetail = (props) =>
 {
     return (
         <>
-            <img src={props.img} alt='Nairobi Sunset' />
+            <img src={props.image} alt='Nairobi Sunset' />
             <h1>Meetup at Nairobi</h1>
             <address>Some address</address>
             <p>We will be meeting up in Nairobi at sunset</p>
