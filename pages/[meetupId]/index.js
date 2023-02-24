13082dfc4824a5bf06ccd2d3f67fe2c4790bf139
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-24 11:22:41 +0300

    Added a call to the getStaticProps function

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -12,4 +12,19 @@ const MeetupDetailsPage = () =>
     )
 }
 
+export async function getStaticProps()
+{
+    return {
+        props: {
+            meetupData: {
+                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/NBO5.jpg/267px-NBO5.jpg',
+                id: 'm1',
+                title: 'Meetup at Nairobi',
+                address: 'Nairobi City, Nairobi',
+                description: 'We will be meeting up in Nairobi at sunset',
+            }
+        }
+    }
+}
+
 export default MeetupDetailsPage
