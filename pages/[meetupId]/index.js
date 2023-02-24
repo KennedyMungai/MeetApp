f14980e59715bc9a5d1105defb7e4891086a8a90
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-24 09:26:51 +0300

    Added some more markup to the component

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -3,6 +3,8 @@ const MeetupDetailsPage = () =>
     return (
         <>
             <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/NBO5.jpg/267px-NBO5.jpg' alt='Nairobi Sunset' />
+            <h1>Meetup at Nairobi</h1>
+            <p>We will be meeting up in Nairobi at sunset</p>
         </>
     )
 }
