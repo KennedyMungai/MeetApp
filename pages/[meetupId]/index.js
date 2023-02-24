fd3dc13565d46cb82d1ad92edb6128cfa038eae3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-24 08:49:42 +0300

    Imported and utilized the NewMeetupForm component

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,7 +1,9 @@
+import NewMeetupForm from "../../components/meetups/NewMeetupForm"
+
 const NewMeetupPage = () =>
 {
     return (
-        <div>NewMeetup</div>
+        <NewMeetupForm />
     )
 }
 
