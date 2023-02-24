b32e54c355c8867086deea6ce8ae85e80eb3ed34
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-24 10:33:00 +0300

    Used the MeetupDetail component instead of hardcoding everything

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,12 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail"
+
 const MeetupDetailsPage = () =>
 {
     return (
-        <>
-            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/NBO5.jpg/267px-NBO5.jpg' alt='Nairobi Sunset' />
-            <h1>Meetup at Nairobi</h1>
-            <address>Some address</address>
-            <p>We will be meeting up in Nairobi at sunset</p>
-        </>
+        <MeetupDetail
+            image='https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/NBO5.jpg/267px-NBO5.jpg'
+            title='Meetup at Nairobi'
+            address='Nairobi City, Nairobi'
+            description='We will be meeting up in Nairobi at sunset'
+        />
     )
 }
 
