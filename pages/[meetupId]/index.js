1d905f853079cbb15bc13f1ad0ac1538aa0f09d2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-24 11:26:17 +0300

    Set the meetupId as the id for meetupData

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -22,7 +22,7 @@ export async function getStaticProps(context)
         props: {
             meetupData: {
                 image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/NBO5.jpg/267px-NBO5.jpg',
-                id: 'm1',
+                id: meetupId,
                 title: 'Meetup at Nairobi',
                 address: 'Nairobi City, Nairobi',
                 description: 'We will be meeting up in Nairobi at sunset',
