822ca65beb0e459eec0133ab98e04dafda2ddc96
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-24 09:25:27 +0300

    Added some more approproate markup to the index of the dynamic route file

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,6 +1,9 @@
 const MeetupDetailsPage = () =>
 {
     return (
+        <>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/NBO5.jpg/267px-NBO5.jpg' alt='Nairobi Sunset' />
+        </>
     )
 }
 
