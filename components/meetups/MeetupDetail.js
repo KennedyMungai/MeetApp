b6033aa99ab71686b458fbc8eb6438eb52644eb5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-24 10:27:36 +0300

    Accepting the img src through props

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -2,7 +2,7 @@ const MeetupDetail = (props) =>
 {
     return (
         <>
-            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/NBO5.jpg/267px-NBO5.jpg' alt='Nairobi Sunset' />
+            <img src={props.img} alt='Nairobi Sunset' />
             <h1>Meetup at Nairobi</h1>
             <address>Some address</address>
             <p>We will be meeting up in Nairobi at sunset</p>
